fc7c612c84beca39904e34220bc25dc9a9657768
subhiksha-ai7 <subhikshasundarapandian@gmail.com>
2026-06-04 21:27:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const display = document.getElementById("display");
-
 function appendValue(value){
     display.value += value;
 }
-
 function clearDisplay(){
     display.value = "";
 }
-
 function deleteLast(){
     display.value = display.value.slice(0, -1);
 }
-
 function calculate(){
     try{
         display.value = eval(display.value);
@@ -20,24 +16,17 @@ function calculate(){
         display.value = "Error";
     }
 }
-
-// Keyboard Support
 document.addEventListener("keydown", function(event){
-
     const key = event.key;
-
     if("0123456789+-*/.%".includes(key)){
         appendValue(key);
     }
-
     if(key === "Enter"){
         calculate();
     }
-
     if(key === "Backspace"){
         deleteLast();
     }
-
     if(key === "Escape"){
         clearDisplay();
     }
